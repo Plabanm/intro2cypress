b994e90f0684c0a80a5bebfdeea4dd5b842264a4
Plaban <Majumder>
2020-02-28 21:05:22 +0000

    test steps

--- cypress/integration/Basic/example.spec.js
+++ cypress/integration/Basic/example.spec.js
@@ -1,6 +1,14 @@
-describe('First basic test', ()=>{
-    it('true should be true', ()=>{
-        expect(true).equal(true)
+describe('Browser Actions', ()=>{
+    it('Should Load Correct URL', ()=>{
+        cy.visit('http://example.com/',{timeout: 10000})
+    })
+
+    it('Should check correct url', ()=>{
+        cy.url().should('include', 'example.com')
+    })
+
+    it('Should check correct element on the page', ()=>{
+        cy.get('h1').should('be.visible')
     })
 
 })
